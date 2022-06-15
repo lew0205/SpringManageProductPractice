7a44bafd5c942995affa0a97515b99dbe0774e40
이은우 <80537533+lew0205@users.noreply.github.com>
2022-06-15 13:57:05 +0900

    Modify Entities

--- src/main/java/com/mp/manage/product/domain/Member.java
+++ src/main/java/com/mp/manage/product/domain/Member.java
@@ -1,10 +1,7 @@
 package com.mp.manage.product.domain;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Member {
@@ -12,7 +9,7 @@ public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+@Column(nullable = false)
     private String name;
 
     private String address;
--- src/main/java/com/mp/manage/product/domain/Order.java
+++ src/main/java/com/mp/manage/product/domain/Order.java
@@ -18,4 +18,36 @@ public class Order {
 
     @Column(nullable = false)
     private Long productNum;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Long getProductNum() {
+        return productNum;
+    }
+
+    public void setProductNum(Long productNum) {
+        this.productNum = productNum;
+    }
 }
