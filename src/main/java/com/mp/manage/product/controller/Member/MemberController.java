69fc995fce978d0a034568405a3d073042e0cbf5
이은우 <80537533+lew0205@users.noreply.github.com>
2022-06-16 23:21:29 +0900

    Modify Controller

--- src/main/java/com/mp/manage/product/controller/Member/MemberController.java
+++ src/main/java/com/mp/manage/product/controller/Member/MemberController.java
@@ -1,11 +1,10 @@
 package com.mp.manage.product.controller.Member;
 
 import com.mp.manage.product.domain.member.Member;
-import com.mp.manage.product.domain.member.MemberForm;
+import com.mp.manage.product.domain.dto.MemberDto;
 import com.mp.manage.product.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,7 +19,7 @@ public class MemberController {
 
     @GetMapping("/member/list")
     @ResponseBody
-    public List<Member> memberList(Model model) {
+    public List<Member> memberList() {
         List<Member> members = memberService.findMembers();
         return members;
     }
@@ -31,7 +30,7 @@ public class MemberController {
     }
 
     @PostMapping("/member/join")
-    public String memberJoin(MemberForm form) {
+    public String memberJoin(MemberDto form) {
         Member member = new Member();
         member.setName(form.getName());
         member.setAddress(form.getAddress());
--- src/main/java/com/mp/manage/product/controller/Product/ProductController.java
+++ src/main/java/com/mp/manage/product/controller/Product/ProductController.java
@@ -1,13 +1,29 @@
 package com.mp.manage.product.controller.Product;
 
+import com.mp.manage.product.domain.product.Product;
+import com.mp.manage.product.domain.dto.ProductDto;
 import com.mp.manage.product.service.ProductService;
-import org.springframework.stereotype.Controller;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
 public class ProductController {
-    private ProductService productService;
+    private final ProductService productService;
 
-    public ProductController(ProductService productService) {
-        this.productService = productService;
+    @GetMapping("product/list")
+    public List<Product> productList(){
+        List<Product> products = productService.findProducts();
+        return products;
+    }
+
+    @PostMapping("/product")
+    public void productJoin(@RequestBody ProductDto productDto){
+
     }
 }
